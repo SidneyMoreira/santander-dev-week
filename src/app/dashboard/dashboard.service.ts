081ffacd52134dff863c3c6efd4b775d02beeba2
KadarPegasus <sidmoreira.hp@gmail.com>
2021-05-30 21:50:48 -0300

    Trocando HTTP -> HTTPS endereço Heroku

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -7,7 +7,7 @@ import Stock from '../shared/models/stock-model';
 })
 export class DashboardService {
 
-  readonly baseUrl = 'http://dio-santander-devweek.herokuapp.com/bootcamp';
+  readonly baseUrl = 'https://dio-santander-devweek.herokuapp.com/bootcamp';
 
   constructor(private http: HttpClient) { }
 
